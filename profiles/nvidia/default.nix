031dd0d017a4d6d9e68cc8a914c1101d90f4c4d1
popipopka <eg.belykh@yandex.ru>
2025-10-26 21:16:07 +0300

    add nvidia profile

--- profiles/nvidia/default.nix
+++ profiles/nvidia/default.nix
@@ -1,13 +1,15 @@
-{ host, ... }: {
+{ host, ... }:
+{
   imports = [
     ../../hosts/${host}
     ../../modules/drivers
     ../../modules/core
   ];
-  # Enable GPU Drivers
-  drivers.amdgpu.enable = false;
+
   drivers.nvidia.enable = true;
-  drivers.nvidia-prime.enable = false;
+
+  drivers.amdgpu.enable = false;
   drivers.intel.enable = false;
   vm.guest-services.enable = false;
 }
+
