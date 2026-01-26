898f69c80489bff3b3f60e0147cc35fffb8b43e6
popipopka <eg.belykh@yandex.ru>
2026-01-26 18:41:30 +0300

    backup

--- hosts/ati/host-services.nix
+++ hosts/ati/host-services.nix
@@ -2,7 +2,11 @@
   services = {
     ollama = {
       enable = true;
+
       openFirewall = true;
+      host = "0.0.0.0";
+
+      acceleration = "cuda";
       loadModels = [
         "aliafshar/gemma3-it-qat-tools:12b"
       ];
